7c65a9212934a4f8a6d02dd37e7ccf8337ba12ee
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-07-04 11:24:21 -0500

    feat(router): create routes using react-router-dom

--- promart-store/src/App.js
+++ promart-store/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
 import './App.sass';
-/* import Login from './components/Login/Login' */
+import Login from './components/Login/Login'
 import Products from './components/Home/Products/Products'
 
 function App() {
   return (
     <div className="app-container">
-      {/* <Login /> */}
-      <Products />
+    <Router>
+        <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/login" component={Login} />
+        <Route path="/products" component={Products}/>
+    </Switch>
+    </Router>
     </div>
   );
 }

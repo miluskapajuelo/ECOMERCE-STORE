c377bf6d0d6110aaa21e994edf4d904b69170d35
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-07-04 11:23:00 -0500

    refactor(component): components in Login are connected

--- promart-store/src/components/Login/input/Input.js
+++ promart-store/src/components/Login/input/Input.js
@@ -10,7 +10,7 @@ const Input = ({attribute, handleChange, param}) => {
             name= {attribute.name}
             placeholder= {attribute.placeholder}
             type= {attribute.type}
-            onChange= {(e)=> handleChange(e.target.name, e.target.value)}
+            onChange= {(e)=> handleChange(e.target.name,e.target.value)}
             className= {param?"regular-error":"regular-style" }
             />
         </div>   

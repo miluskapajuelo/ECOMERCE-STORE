7bb55ea1b74fb38a508156ae9187e41f4b01962f
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-07-04 19:38:05 -0500

    feat(component): create component product + products

---
+++ promart-store/src/components/Home/Product/product.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Product.sass'
+import accounting from 'accounting';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faHeart} from '@fortawesome/free-solid-svg-icons'
+
+const Product = ({product}) => {
+
+
+    return (
+        <section className="card-container">
+            <article className="first-content">
+                <div>30% OFF</div>
+                <div><FontAwesomeIcon icon={faHeart} /></div>
+            </article>
+            <figure className="second-content">
+                <img src={product.image} alt={product.title} width="60%" height="100%" />
+            </figure>
+            <article className="third-content">
+                <div>{product.title}</div>
+                <div className="price">{accounting.formatMoney(product.price)}</div>
+            </article>
+        </section>
+    )
+}
+
+export default Product

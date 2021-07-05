85a2404f1c962e80a4412d9e7ddf6dd8f66bab33
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-07-04 23:56:05 -0500

    feat(component): create + route components with footer

---
+++ promart-store/src/components/Footer/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faHeart} from '@fortawesome/free-solid-svg-icons'
+import {faHome} from '@fortawesome/free-solid-svg-icons'
+import {faShoppingCart} from '@fortawesome/free-solid-svg-icons'
+import {faUser} from '@fortawesome/free-solid-svg-icons'
+import './Footer.sass'
+import {useHistory} from 'react-router-dom'
+
+
+const Footer = () => {
+
+    let history = useHistory()
+    const shoppingCart = ()=>{
+        history.push("/cart")
+    }
+    const home = ()=>{
+        history.push("/products")
+    }
+    return (
+        <footer>
+            <article className="section-button"><button className="button" onClick={home}><FontAwesomeIcon icon={faHome} /> Home</button></article>
+            <article className="section-icons">
+                <div className="icon"><FontAwesomeIcon icon={faHeart} /></div>
+                <div className="icon" onClick={shoppingCart}><FontAwesomeIcon icon={faShoppingCart} /></div>
+                <div className="icon"><FontAwesomeIcon icon={faUser} /></div>
+            </article>
+        </footer>
+    )
+}
+
+
+export default Footer

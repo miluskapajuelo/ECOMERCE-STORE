4366e3b9c9a40a253165676d7d04b317de6e80c7
Miluska Pajuelo <miluskapajuelo@gmail.com>
2021-07-08 12:36:41 -0500

    feat(module): connect useContext with localStorage

--- promart-store/src/module/reducer.js
+++ promart-store/src/module/reducer.js
@@ -1,25 +1,42 @@
+import {getItemCart} from './../module/cart-local'
+
 export const initialState = {
     basket:[],
 }
 
 const reducer =(state, action)=>{
-    console.log(action);
     switch(action.type){
         case 'add':
+            localStorage.setItem("cart", JSON.stringify([...state.basket, action.item]));
             return {
                 ...state,
                 basket:[...state.basket, action.item]
             }
+            case 'selected':
+                localStorage.setItem("selectedCart", JSON.stringify([...state.basket, action.item]));
+                return {
+                    ...state,
+                    basket:[...state.basket, action.item]
+                }
+        case 'reduce':
+            localStorage.removeItem(JSON.stringify([action.item]));
+            return {
+                ...state,
+                basket:[...state.basket, action.item]
+            }
+        case 'initial':
+            return {
+                ...state,
+                basket:[...getItemCart()]
+            }
         case 'remove':
+            localStorage.removeItem(action.item)
             let newCart =[...state.basket]
-            console.log('statebasket',state.basket)
-            console.log('action.id',action.id)
-            const index = state.basket.findIndex((basketItem)=>basketItem.id ===action.id)
-            console.log('index',index)
-            if(index>0){
-                newCart.splice(index,1)
-            }else{
-                console.log('error to remove')
+            const index = state.basket.findIndex(element=>element.id ===action.item.id)
+            newCart.splice(index,1)
+            return {
+                ...state,
+                basket:[...newCart]
             }
     }
 }
